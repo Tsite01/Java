5afe2f250a087affe06b69a23090eb7c1b2d35c7
Tsite01 <110942700+Tsite01@users.noreply.github.com>
2023-06-03 00:14:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function calculateDiameter() {
+    var radiusInput = document.getElementById("radius");
+    var radius = parseFloat(radiusInput.value);
+    if (isNaN(radius)) {
+        document.getElementById("result").textContent = "Некорректный ввод. Пожалуйста, введите число.";
+    } else {
+        var diameter = 2 * radius;
+        document.getElementById("result").textContent = "Диаметр окружности равен: " + diameter;
+    }
+}
